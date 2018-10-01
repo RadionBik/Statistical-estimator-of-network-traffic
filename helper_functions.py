e8f6df8ac304da177f6c7d1a96bec424c2e14d6b
Radion Bikmukhamedov <radion.bikmukhamedov@gmail.com>
2018-10-01 13:54:04 +0300

    mv svg

--- helper_functions.py
+++ helper_functions.py
@@ -452,7 +452,7 @@ def plot_hist_kde_em(traffic, kde_estimators, em_estimators=None, logScale=True,
                 fig.tight_layout()
                 fig.subplots_adjust(top=0.88)
         if saveToFile:
-            figure_name = 'hist_'+mod_addr(device).replace(' ','_')+'_'+'.svg'
+            figure_name = 'stat_figures'+os.sep+'hist_'+mod_addr(device).replace(' ','_')+'_'+'.svg'
             print('Saved figure as {}'.format(figure_name))
             plt.savefig(figure_name)
             # else:
