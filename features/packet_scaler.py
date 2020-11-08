976ff755f2974ae54c9990fee3257a498306dd2d
Radion Bikmukhamedov <radion.bikmukhamedov@pm.me>
2020-11-08 17:05:37 +0300

    fix scaler when IAT==1 ms

--- features/packet_scaler.py
+++ features/packet_scaler.py
@@ -13,14 +13,14 @@ class PacketScaler:
         packet_pairs[:, 0] = packet_pairs[:, 0] / self.max_packet_len
         # avoids warning and -inf values. the scale here is in microseconds (?)
         zero_iats = np.isclose(packet_pairs[:, 1], 0.)
-        packet_pairs[:, 1][zero_iats] = 0
+        packet_pairs[:, 1][zero_iats] = -1
         packet_pairs[:, 1][~zero_iats] = np.log10(packet_pairs[:, 1][~zero_iats])
         return packet_pairs
 
     def inverse_transform(self, packet_pairs):
         packet_pairs[:, 0] = packet_pairs[:, 0] * self.max_packet_len
         # to correctly rescale, we need to know which were initially zeros
-        zero_iats = np.isclose(packet_pairs[:, 1], 0., atol=1e-3)
+        zero_iats = np.isclose(packet_pairs[:, 1], -1, atol=1e-3)
         packet_pairs[:, 1][zero_iats] = 0
         packet_pairs[:, 1][~zero_iats] = 10 ** packet_pairs[:, 1][~zero_iats]
         return packet_pairs
