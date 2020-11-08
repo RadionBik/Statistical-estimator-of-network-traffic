fd5c8ea62623b7b26c4352f83a9a325038fcb2fc
Radion Bikmukhamedov <radion.bikmukhamedov@pm.me>
2020-11-08 15:30:15 +0300

    add neptune to markov model

--- markov_baseline/train_evaluate_markov.py
+++ markov_baseline/train_evaluate_markov.py
@@ -1,5 +1,8 @@
 import argparse
 
+import neptune
+
+import settings
 import stat_metrics
 from features.data_utils import load_train_test_dataset, quantize_datatset, restore_features
 from features.evaluation import evaluate_traffic
@@ -19,6 +22,12 @@ def _parse_args():
         help='path to the quantizer checkpoint',
         required=True
     )
+    parser.add_argument(
+        '--log_neptune',
+        dest='log_neptune',
+        action='store_true',
+        default=False
+    )
     return parser.parse_args()
 
 
@@ -35,6 +44,15 @@ def main():
     seq_metrics = stat_metrics.calc_stats(test_states, sampled)
     gen_df = restore_features(quantizer, sampled)
     eval_metrics = evaluate_traffic(gen_df, test_df)
+    if args.log_neptune:
+        neptune.init(
+            settings.NEPTUNE_PROJECT,
+            settings.NEPTUNE_API_TOKEN,
+        )
+        neptune.create_experiment(name='markov_model', params=vars(args))
+        for name, value in dict(**seq_metrics, **eval_metrics).items():
+            neptune.log_metric(name, value)
+        neptune.stop()
 
 
 if __name__ == '__main__':
