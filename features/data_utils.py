7c6a2c210cddfc195db35e37e24f78f4c1800e55
Radion Bikmukhamedov <radion.bikmukhamedov@pm.me>
2020-11-08 15:17:01 +0300

    add outlier removing at ds loading

--- features/data_utils.py
+++ features/data_utils.py
@@ -3,8 +3,18 @@ import pandas as pd
 from pcap_parsing.parsed_fields import select_features, ParsedFields
 
 
-def load_train_test_dataset(path, max_rows=10_000):
+def _filter_iat_outliers(df):
+    q_low = df[ParsedFields.iat].quantile(0.01)
+    q_hi = df[ParsedFields.iat].quantile(0.99)
+
+    return df[(df[ParsedFields.iat] < q_hi) & (df[ParsedFields.iat] > q_low)]
+
+
+def load_train_test_dataset(path, max_rows=10_000, filter_iat_outliers=True):
     extr_stats = pd.read_csv(path, nrows=max_rows)
+    if filter_iat_outliers:
+        extr_stats = _filter_iat_outliers(extr_stats)
+
     train_size = len(extr_stats) - len(extr_stats) // 3
     train_df, test_df = extr_stats.iloc[:train_size], extr_stats.iloc[train_size:]
     return train_df, test_df
